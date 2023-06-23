947075b3d9b01a99d6b5c769a1a69ce7946e5224
Marat <Marat@DESKTOP-2R5S4N5>
2023-06-23 22:51:53 +0300

    Добавьте файлы проекта.

---
+++ ClientTCP/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ClientTCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            try
+            {
+                SendMessage(port, ip);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally 
+            {
+                Console.WriteLine();
+            }
+
+        }
+
+        static void SendMessage(int port, string ip)
+        {
+            
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Enter the message:");
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+
+
+            size = tcpSocket.Receive(buffer);
+            Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, size));
+
+            if (message.IndexOf(">>End") == -1)
+                SendMessage(port, ip);
+
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+        }
+    }
+}
---
+++ ServerTCP/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerTCP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            try
+            {
+                tcpSocket.Bind(tcpEndPoint);
+                tcpSocket.Listen(5);
+
+                while (true)
+                {
+                    var listener = tcpSocket.Accept();
+                    var buffer = new byte[256];
+                    var size = 0;
+                    string data = null;
+
+                    size = listener.Receive(buffer);
+
+                    data += Encoding.UTF8.GetString(buffer, 0, size);
+
+                    Console.WriteLine(data);
+
+                    listener.Send(Encoding.UTF8.GetBytes("Success"));
+
+                    if (data.IndexOf(">>End") > 1)
+                    {
+                        Console.WriteLine("Server shutdowned");
+                        break;
+                    }
+
+                    listener.Shutdown(SocketShutdown.Both);
+                    listener.Close();
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
